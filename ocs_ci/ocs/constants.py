f5bb0dba043f20cdc43faaef7f49f852e37ba390
Shylesh Kumar Mohan <shmohan@redhat.com>
2020-07-31 15:29:23 -0700

    Rename all 'independent' to 'external'
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>

--- ocs_ci/ocs/constants.py
+++ ocs_ci/ocs/constants.py
@@ -156,7 +156,7 @@ CODESPEED_URL = 'http://10.0.78.167:8000/'
 UPI_INSTALL_SCRIPT = "upi_on_aws-install.sh"
 
 DEFAULT_CLUSTERNAME = 'ocs-storagecluster'
-DEFAULT_CLUSTERNAME_INDEPENDENT_MODE = 'ocs-independent-storagecluster'
+DEFAULT_CLUSTERNAME_EXTERNAL_MODE = 'ocs-external-storagecluster'
 DEFAULT_BLOCKPOOL = f'{DEFAULT_CLUSTERNAME}-cephblockpool'
 METADATA_POOL = f'{DEFAULT_CLUSTERNAME}-cephfilesystem-metadata'
 DATA_POOL = f'{DEFAULT_CLUSTERNAME}-cephfilesystem-data0'
@@ -180,11 +180,11 @@ DEFAULT_STORAGECLASS_RBD = f'{DEFAULT_CLUSTERNAME}-ceph-rbd'
 DEFAULT_STORAGECLASS_LSO = 'localblock'
 
 # Default StorageClass for Independent-mode
-DEFAULT_INDEPENDENT_MODE_STORAGECLASS_CEPHFS = (
-    f'{DEFAULT_CLUSTERNAME_INDEPENDENT_MODE}-cephfs'
+DEFAULT_EXTERNAL_MODE_STORAGECLASS_CEPHFS = (
+    f'{DEFAULT_CLUSTERNAME_EXTERNAL_MODE}-cephfs'
 )
-DEFAULT_INDEPENDENT_MODE_STORAGECLASS_RBD = (
-    f'{DEFAULT_CLUSTERNAME_INDEPENDENT_MODE}-ceph-rbd'
+DEFAULT_EXTERNAL_MODE_STORAGECLASS_RBD = (
+    f'{DEFAULT_CLUSTERNAME_EXTERNAL_MODE}-ceph-rbd'
 )
 
 # encoded value of 'admin'
@@ -500,12 +500,12 @@ STORAGE_CLUSTER_YAML = os.path.join(
     TEMPLATE_DEPLOYMENT_DIR, "storage-cluster.yaml"
 )
 
-INDEPENDENT_STORAGE_CLUSTER_YAML = os.path.join(
-    TEMPLATE_DEPLOYMENT_DIR, "independent-storage-cluster.yaml"
+EXTERNAL_STORAGE_CLUSTER_YAML = os.path.join(
+    TEMPLATE_DEPLOYMENT_DIR, "external-storage-cluster.yaml"
 )
 
-INDEPENDENT_CLUSTER_SECRET_YAML = os.path.join(
-    TEMPLATE_DEPLOYMENT_DIR, "independent-cluster-secret.yaml"
+EXTERNAL_CLUSTER_SECRET_YAML = os.path.join(
+    TEMPLATE_DEPLOYMENT_DIR, "external-cluster-secret.yaml"
 )
 
 OPERATOR_SOURCE_SECRET_YAML = os.path.join(
--- tests/helpers.py
+++ tests/helpers.py
@@ -435,10 +435,10 @@ def default_storage_class(
     Returns:
         OCS: Existing StorageClass Instance
     """
-    independent = config.DEPLOYMENT['independent_mode']
+    external = config.DEPLOYMENT['external_mode']
     if interface_type == constants.CEPHBLOCKPOOL:
-        if independent:
-            resource_name = constants.DEFAULT_INDEPENDENT_MODE_STORAGECLASS_RBD
+        if external:
+            resource_name = constants.DEFAULT_EXTERNAL_MODE_STORAGECLASS_RBD
         else:
             resource_name = constants.DEFAULT_STORAGECLASS_RBD
         base_sc = OCP(
@@ -446,8 +446,8 @@ def default_storage_class(
             resource_name=resource_name
         )
     elif interface_type == constants.CEPHFILESYSTEM:
-        if independent:
-            resource_name = constants.DEFAULT_INDEPENDENT_MODE_STORAGECLASS_CEPHFS
+        if external:
+            resource_name = constants.DEFAULT_EXTERNAL_MODE_STORAGECLASS_CEPHFS
         else:
             resource_name = constants.DEFAULT_STORAGECLASS_CEPHFS
         base_sc = OCP(
